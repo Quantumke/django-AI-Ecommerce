8cd8d2197bb2923c9b7842247ba00165955ceb44
quantumke <nguruben@gmail.com>
2016-05-28 19:00:13 +0300

    ignore refs

---
+++ shop/migrations/0002_initial.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Category'
+        db.create_table('categories', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=100)),
+            ('description', self.gf('django.db.models.fields.TextField')()),
+            ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('meta_keywords', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('meta_description', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('updated_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'shop', ['Category'])
+
+        # Adding model 'Product'
+        db.create_table('products', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(unique=True, max_length=200)),
+            ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=200)),
+            ('brand', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('sku', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('price', self.gf('django.db.models.fields.DecimalField')(max_digits=9, decimal_places=2)),
+            ('old_price', self.gf('django.db.models.fields.DecimalField')(default=0.0, max_digits=9, decimal_places=2, blank=True)),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
+            ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('is_bestseller', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('quantity', self.gf('django.db.models.fields.IntegerField')()),
+            ('description', self.gf('django.db.models.fields.TextField')()),
+            ('meta_keywords', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('meta_description', self.gf('django.db.models.fields.CharField')(max_length=500)),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('updated_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'shop', ['Product'])
+
+        # Adding M2M table for field categories on 'Product'
+        m2m_table_name = db.shorten_name('products_categories')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('product', models.ForeignKey(orm[u'shop.product'], null=False)),
+            ('category', models.ForeignKey(orm[u'shop.category'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['product_id', 'category_id'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Category'
+        db.delete_table('categories')
+
+        # Deleting model 'Product'
+        db.delete_table('products')
+
+        # Removing M2M table for field categories on 'Product'
+        db.delete_table(db.shorten_name('products_categories'))
+
+
+    models = {
+        u'shop.category': {
+            'Meta': {'ordering': "['-created_at']", 'object_name': 'Category', 'db_table': "'categories'"},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'meta_description': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'meta_keywords': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '100'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
+        },
+        u'shop.product': {
+            'Meta': {'ordering': "['-created_at']", 'object_name': 'Product', 'db_table': "'products'"},
+            'brand': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'categories': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['shop.Category']", 'symmetrical': 'False'}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'description': ('django.db.models.fields.TextField', [], {}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_bestseller': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'meta_description': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            'meta_keywords': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '200'}),
+            'old_price': ('django.db.models.fields.DecimalField', [], {'default': '0.0', 'max_digits': '9', 'decimal_places': '2', 'blank': 'True'}),
+            'price': ('django.db.models.fields.DecimalField', [], {'max_digits': '9', 'decimal_places': '2'}),
+            'quantity': ('django.db.models.fields.IntegerField', [], {}),
+            'sku': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '200'}),
+            'updated_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['shop']
